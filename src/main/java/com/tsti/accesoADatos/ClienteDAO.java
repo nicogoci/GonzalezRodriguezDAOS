2cdbb94ca342870e348835c1f6b6072efe8fbbbc
Ale <Ale@DESKTOP-CGRH2FS>
2023-06-26 22:57:59 -0300

    ClienteResponseDTO y ClienteDAO

---
+++ src/main/java/com/tsti/accesoADatos/ClienteDAO.java
@@ -0,0 +1,15 @@
+package com.tsti.accesoADatos;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.tsti.entidades.Persona;
+
+public class ClienteDAO {
+	
+
+
+}
