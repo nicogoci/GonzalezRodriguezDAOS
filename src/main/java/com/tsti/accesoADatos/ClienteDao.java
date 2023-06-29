adde40be6e98715b1595220b665b2ee185872637
Ale <Ale@DESKTOP-CGRH2FS>
2023-06-29 09:49:26 -0300

    ClienteDao

---
+++ src/main/java/com/tsti/accesoADatos/ClienteDao.java
@@ -0,0 +1,19 @@
+package com.tsti.accesoADatos;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.tsti.entidades.Cliente;
+import com.tsti.entidades.Persona;
+
+public interface ClienteDao  extends JpaRepository<Cliente, Long> {
+	
+	@Query("SELECT c FROM Cliente c WHERE c.nombre like '%?1%'")
+	Collection<Cliente> findClienteLike(String parte);
+	
+	public List<Cliente> findByApellidoOrNombre(String apellido, String nombre);
+
+}
