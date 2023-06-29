482c2cbf39334fd48bc6d3d82ae7efccbf075207
Ale <Ale@DESKTOP-CGRH2FS>
2023-06-29 09:55:54 -0300

    ClienteDao

--- src/main/java/com/tsti/accesoADatos/ClienteDao.java
+++ src/main/java/com/tsti/accesoADatos/ClienteDao.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.tsti.entidades.Cliente;
-import com.tsti.entidades.Persona;
+
 
 public interface ClienteDao  extends JpaRepository<Cliente, Long> {
 	
